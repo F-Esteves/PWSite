cca254f7d3eef7e83d54e77fcddb15afd4318fed
F-Esteves <francisco.jp.esteves@outlook.pt>
2021-01-21 07:11:21 +0000

    Update users.sql

--- PWSite V1.3/users.sql
+++ PWSite V1.3/users.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS `users` (
   `username` varchar(20) NOT NULL,
   `password` varchar(20) NOT NULL,
-     `email` varchar(40) NOT NULL,
   PRIMARY KEY (`username`)
 ) ENGINE=InnoDB CHARSET=utf8;
